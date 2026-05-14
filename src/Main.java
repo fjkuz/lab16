09384f5e65877ada1b3dd3c5c422c0f0410d4261
fjkuuz <fjkuz@icloud.com>
2026-05-14 09:37:56 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,15 @@ import java.io.*;
 // Необходимо программно удалить комментарии и переписать программный код в другой файл.
 public class Main{
     public static  void main(String[]args){
-        try
-        {
-            BufferedReader br=new BufferedReader(new FileReader("C:/lab/lab16.txt"));
+        try(BufferedReader br=new BufferedReader(new FileReader("C:/lab/lab16.txt"));
+            BufferedWriter bw=new BufferedWriter(new FileWriter("C/lab/lab162.txt"))){
             StringBuilder sb = new StringBuilder();
             String str;
             while((str=br.readLine())!=null){
                 sb.append(str).append("\n");
             }
-            br.close();
             String rez = sb.toString().replaceAll("//.*|/\\*(\\s|.)*?\\s\\*/","");
-            File file =new File("C:/lab/lab162.txt");
-            BufferedWriter bw=new BufferedWriter(new FileWriter(file));
             bw.write(rez);
-            bw.close();
             System.out.println("комментарии удалены");
         }
         catch (IOException ex) {
